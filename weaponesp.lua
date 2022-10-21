518f16ac51475b0ea4a4a5d2455037cc2e7e645f
iot#5857 <73656173+iotxv@users.noreply.github.com>
2022-10-21 20:00:01 +0200

    Create weaponesp.lua

---
+++ weaponesp.lua
@@ -0,0 +1,88 @@
+weaponesp = {}
+
+local c = workspace.CurrentCamera
+local ps = game:GetService("Players")
+local lp = ps.LocalPlayer
+local rs = game:GetService("RunService")
+
+local function ftool(cr)
+    for a,b in next, cr:GetChildren() do 
+        if b.ClassName == 'Tool' then
+            return tostring(b.Name)
+        end
+    end
+    return 'empty'
+end
+
+local function esp(p,cr)
+    local h = cr:WaitForChild("Humanoid")
+    local hrp = cr:WaitForChild("HumanoidRootPart")
+
+    local text = Drawing.new('Text')
+    text.Visible = false
+    text.Center = true
+    text.Outline = true
+    text.Color = Color3.new(1,1,1)
+    text.Font = 2
+    text.Size = 13
+
+    local c1 
+    local c2
+    local c3 
+
+    local function dc()
+        text.Visible = false
+        text:Remove()
+        if c3 then
+            c1:Disconnect()
+            c2:Disconnect()
+            c3:Disconnect()
+            c1 = nil 
+            c2 = nil
+            c3 = nil
+        end
+    end
+
+    c2 = cr.AncestryChanged:Connect(function(_,parent)
+        if not parent then
+            dc()
+        end
+    end)
+
+    c3 = h.HealthChanged:Connect(function(v)
+        if (v<=0) or (h:GetState() == Enum.HumanoidStateType.Dead) then
+            dc()
+        end
+    end)
+
+    c1 = rs.Heartbeat:Connect(function()
+        local hrp_pos,hrp_os = c:WorldToViewportPoint(hrp.Position)
+        if hrp_os then
+            text.Position = Vector2.new(hrp_pos.X, hrp_pos.Y)
+            text.Text = '[ '..tostring(ftool(cr))..' ]'
+            text.Visible = true
+        else
+            text.Visible = false
+        end
+    end)
+
+end
+
+local function p_added(p)
+    if p.Character then
+        esp(p,p.Character)
+    end
+    p.CharacterAdded:Connect(function(cr)
+        esp(p,cr)
+    end)
+end
+
+for a,b in next, ps:GetPlayers() do 
+    if b ~= lp then
+        p_added(b)
+    end
+end
+
+ps.PlayerAdded:Connect(p_added)
+
+return weaponesp
